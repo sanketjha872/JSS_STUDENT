a78b22abdcb386900a5de5b113c2d18a2b4f270e
Sanket <sanketjha116@gmail.com>
2026-03-10 12:31:39 +0530

    Last change

--- app/src/main/java/com/jhainusa/jss_student/SplashScreen.kt
+++ app/src/main/java/com/jhainusa/jss_student/SplashScreen.kt
@@ -1,21 +1,25 @@
 package com.jhainusa.jss_student
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun SplashScreen(){
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().background(Color.White),
         contentAlignment = Alignment.Center
     ){
         Image(
-            painter = painterResource(R.drawable.chatgpt_image_jul_28__2025__02_56_35_am),
+            painter = painterResource(R.drawable.logo),
             contentDescription = null
         )
     }
